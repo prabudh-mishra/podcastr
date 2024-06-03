89b01f95f091711a7462b1f656d62b12ce937baa
Prabudh Mishra <prabudh.mishra18@gmail.com>
2024-06-03 22:41:48 +0200

    Initialised Folder structure and created Left sidebar

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,29 @@
+import LeftSidebar from "@/components/LeftSidebar";
+import Image from "next/image";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="relative flex flex-col">
+      <main className="relative flex bg-black-3">
+        <LeftSidebar />
+        <section>
+          <div>
+            <div>
+              <Image />
+              MobileNav
+            </div>
+            <div>
+              Toaster (notification popups)
+              {children}
+            </div>
+          </div>
+        </section>
+        <p className="text-white-1">RIGHT SIDEBAR</p>
+      </main>
+    </div>
+  );
+}
