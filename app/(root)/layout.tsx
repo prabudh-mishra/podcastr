9c2b83bbfa4a7cf8e86052978b927a275e00633a
Prabudh Mishra <prabudh.mishra18@gmail.com>
2024-06-03 22:57:01 +0200

    Started Right navigation and main page components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
 import LeftSidebar from "@/components/LeftSidebar";
+import MobileNav from "@/components/MobileNav";
+import RightSidebar from "@/components/RightSidebar";
 import Image from "next/image";
 
 export default function RootLayout({
@@ -10,19 +12,25 @@ export default function RootLayout({
     <div className="relative flex flex-col">
       <main className="relative flex bg-black-3">
         <LeftSidebar />
-        <section>
-          <div>
-            <div>
-              <Image />
-              MobileNav
+
+        <section className="flex min-h-screen flex-1 flex-col sm:px-14">
+          <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
+            <div className="flex h-16 items-center justify-between md:hidden">
+              <Image
+                src="/icons/logo.svg"
+                width={30}
+                height={30}
+                alt="Menu icon"
+              />
+              <MobileNav />
             </div>
-            <div>
+            <div className="flex flex-col md:pb-14">
               Toaster (notification popups)
               {children}
             </div>
           </div>
         </section>
-        <p className="text-white-1">RIGHT SIDEBAR</p>
+        <RightSidebar />
       </main>
     </div>
   );
