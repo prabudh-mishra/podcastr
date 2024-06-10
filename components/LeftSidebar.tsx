10ac759a46598bf22f784f32f85a884780cc143f
Prabudh Mishra <prabudh.mishra18@gmail.com>
2024-06-10 15:35:27 +0200

    Created Mobile navigation for smaller devices

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -8,28 +8,34 @@ import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 import { Button } from "./ui/button";
+import { useAudio } from "@/providers/AudioProvider";
 
 const LeftSidebar = () => {
   const pathname = usePathname();
   const router = useRouter();
   const { signOut } = useClerk();
+  const { audio } = useAudio();
 
   return (
-    <section className="left_sidebar">
-      <nav>
+    <section
+      className={cn("left_sidebar h-[calc(100vh-5px)]", {
+        "h-[calc(100vh-140px)]": audio?.audioURL,
+      })}
+    >
+      <nav className="flex flex-col gap-6">
         <Link
           href="/"
           className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center"
         >
-          <Image src="/icons/logo.svg" alt="Logo" width={23} height={27} />
-          <h1 className="text-24 font-extrabold text-white-1 max-lg:hidden">
+          <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
+          <h1 className="text-24 font-extrabold text-white max-lg:hidden">
             Podcastr
           </h1>
         </Link>
 
         {sidebarLinks.map(({ route, label, imgURL }) => {
           const isActive =
-            pathname === route || pathname.startsWith(`/${route}/`);
+            pathname === route || pathname.startsWith(`${route}/`);
 
           return (
             <Link
@@ -48,7 +54,6 @@ const LeftSidebar = () => {
           );
         })}
       </nav>
-
       <SignedOut>
         <div className="flex-center w-full pb-14 max-lg:px-4 lg:pr-8">
           <Button asChild className="text-16 w-full bg-orange-1 font-extrabold">
@@ -59,10 +64,10 @@ const LeftSidebar = () => {
       <SignedIn>
         <div className="flex-center w-full pb-14 max-lg:px-4 lg:pr-8">
           <Button
-            className="text-16 w-full bg-orange-1 font-extrabold text-white-1"
+            className="text-16 w-full bg-orange-1 font-extrabold"
             onClick={() => signOut(() => router.push("/"))}
           >
-            Sign out
+            Log Out
           </Button>
         </div>
       </SignedIn>
